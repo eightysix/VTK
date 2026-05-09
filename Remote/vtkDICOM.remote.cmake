1caa513d0ed9269bbfe651f6246740ed10a36dfa
eightysix <eighty_._six@icloud.com>
2026-05-09 13:34:34 +0200

    Tweaked remote module format

--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -4,6 +4,6 @@
 
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
-  GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  GIT_TAG master
+  GIT_REPOSITORY "https://github.com/dgobbi/vtk-dicom"
+  GIT_TAG "master"
   )
