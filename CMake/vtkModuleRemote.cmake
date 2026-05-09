fc58062d56aa708df132979c6d1f5177b610677f
eightysix <eighty_._six@icloud.com>
2026-05-09 12:48:22 +0200

    Revert "Tweaked remote module resolution"
    This reverts commit 20eec77ba0e22403a5479b4e0b75f6f2c47332c7.

--- CMake/vtkModuleRemote.cmake
+++ CMake/vtkModuleRemote.cmake
@@ -9,13 +9,8 @@ function(_git_clone git_executable git_repository git_tag module_dir)
     ERROR_VARIABLE git_clone_error
     )
   if(error_code)
-    # Parallel configure steps may race on the first clone of a remote module.
-    # If another process created the directory first, let update logic handle it.
-    if(IS_DIRECTORY "${module_dir}")
-      return()
-    endif()
     message(FATAL_ERROR "Failed to clone repository: '${git_repository}' Clone error is: '${git_clone_error}'")
-  endif()
+  Endif()
 
   execute_process(
     COMMAND "${git_executable}" checkout ${git_tag}
@@ -50,64 +45,14 @@ endfunction()
 # Helper to perform a git update.  Checks the current Git revision against the
 # desired revision and only performs a fetch and checkout if needed.
 function(_git_update git_executable git_repository git_tag module_dir)
-  set(_git_tag_for_fetch "${git_tag}")
-  if("${_git_tag_for_fetch}" MATCHES "^origin/(.+)$")
-    set(_git_tag_for_fetch "${CMAKE_MATCH_1}")
-  endif()
-
   execute_process(
-    COMMAND "${git_executable}" rev-parse --verify "${git_tag}^{commit}"
+    COMMAND "${git_executable}" rev-parse --verify ${git_tag}
     WORKING_DIRECTORY "${module_dir}"
     RESULT_VARIABLE error_code
     OUTPUT_VARIABLE tag_hash
-    ERROR_VARIABLE git_tag_error
     )
   if(error_code)
-    # The ref may not be available yet (e.g., concurrent clone/fetch timing).
-    execute_process(
-      COMMAND "${git_executable}" fetch origin "${_git_tag_for_fetch}"
-      WORKING_DIRECTORY "${module_dir}"
-      RESULT_VARIABLE fetch_error
-      OUTPUT_QUIET
-      ERROR_QUIET
-      )
-    if(fetch_error)
-      execute_process(
-        COMMAND "${git_executable}" fetch origin
-        WORKING_DIRECTORY "${module_dir}"
-        RESULT_VARIABLE fetch_error
-        OUTPUT_QUIET
-        ERROR_QUIET
-        )
-    endif()
-    if(fetch_error)
-      execute_process(
-        COMMAND "${git_executable}" fetch "${git_repository}" "${_git_tag_for_fetch}"
-        WORKING_DIRECTORY "${module_dir}"
-        RESULT_VARIABLE fetch_error
-        OUTPUT_QUIET
-        ERROR_QUIET
-        )
-    endif()
-    execute_process(
-      COMMAND "${git_executable}" rev-parse --verify "${git_tag}^{commit}"
-      WORKING_DIRECTORY "${module_dir}"
-      RESULT_VARIABLE error_code
-      OUTPUT_VARIABLE tag_hash
-      ERROR_VARIABLE git_tag_error
-      )
-    if(error_code AND NOT "${_git_tag_for_fetch}" STREQUAL "${git_tag}")
-      execute_process(
-        COMMAND "${git_executable}" rev-parse --verify "${_git_tag_for_fetch}^{commit}"
-        WORKING_DIRECTORY "${module_dir}"
-        RESULT_VARIABLE error_code
-        OUTPUT_VARIABLE tag_hash
-        ERROR_VARIABLE git_tag_error
-        )
-    endif()
-  endif()
-  if(error_code)
-    message(FATAL_ERROR "Failed to get the hash for tag '${git_tag}' in '${module_dir}'. Git error is: '${git_tag_error}'")
+    message(FATAL_ERROR "Failed to get the hash for tag '${module_dir}'")
   endif()
   execute_process(
     COMMAND "${git_executable}" rev-parse --verify HEAD
@@ -122,7 +67,7 @@ function(_git_update git_executable git_repository git_tag module_dir)
   # Is the hash checkout out that we want?
   if(NOT (tag_hash STREQUAL head_hash))
     execute_process(
-      COMMAND "${git_executable}" fetch origin
+      COMMAND "${git_executable}" fetch "${git_repository}"
       WORKING_DIRECTORY "${module_dir}"
       RESULT_VARIABLE error_code
       )
@@ -135,13 +80,6 @@ function(_git_update git_executable git_repository git_tag module_dir)
       WORKING_DIRECTORY "${module_dir}"
       RESULT_VARIABLE error_code
       )
-    if(error_code AND NOT "${_git_tag_for_fetch}" STREQUAL "${git_tag}")
-      execute_process(
-        COMMAND "${git_executable}" checkout ${_git_tag_for_fetch}
-        WORKING_DIRECTORY "${module_dir}"
-        RESULT_VARIABLE error_code
-        )
-    endif()
     if(error_code)
       message(FATAL_ERROR "Failed to checkout tag: '${git_tag}'")
     endif()
@@ -164,42 +102,13 @@ function(_fetch_with_git git_executable git_repository git_tag module_dir)
     message(FATAL_ERROR "Tag or repository for git checkout should not be empty.")
   endif()
 
-  # If we have a stale empty directory from an interrupted clone, clean it up.
-  if(EXISTS "${module_dir}" AND NOT EXISTS "${module_dir}/.git")
-    file(GLOB _module_dir_contents "${module_dir}/*")
-    if(NOT _module_dir_contents)
-      file(REMOVE_RECURSE "${module_dir}")
-    endif()
-  endif()
-
   # If we don't have a clone yet.
   if(NOT EXISTS "${module_dir}")
     _git_clone("${git_executable}" "${git_repository}" "${git_tag}" "${module_dir}")
-    if(EXISTS "${module_dir}/.git")
-      message(STATUS " The remote module: ${git_repository} is cloned into the directory ${module_dir}")
-    endif()
+    message(STATUS " The remote module: ${git_repository} is cloned into the directory ${module_dir}")
+  else() # We already have a clone, but we need to check that it has the right revision.
+    _git_update("${git_executable}" "${git_repository}" "${git_tag}" "${module_dir}")
   endif()
-  # If another process is cloning this repository concurrently, wait for .git.
-  set(_wait_for_git 0)
-  while(NOT EXISTS "${module_dir}/.git" AND _wait_for_git LESS 30)
-    execute_process(COMMAND "${CMAKE_COMMAND}" -E sleep 1)
-    math(EXPR _wait_for_git "${_wait_for_git} + 1")
-  endwhile()
-  if(NOT EXISTS "${module_dir}/.git")
-    # Retry once if we ended up with an empty directory.
-    if(EXISTS "${module_dir}")
-      file(GLOB _module_dir_contents "${module_dir}/*")
-      if(NOT _module_dir_contents)
-        file(REMOVE_RECURSE "${module_dir}")
-        _git_clone("${git_executable}" "${git_repository}" "${git_tag}" "${module_dir}")
-      endif()
-    endif()
-  endif()
-  if(NOT EXISTS "${module_dir}/.git")
-    message(FATAL_ERROR "Remote module directory '${module_dir}' exists, but it is not a valid git checkout.")
-  endif()
-  # We already have a clone (or a concurrent configure step created it), so check revision.
-  _git_update("${git_executable}" "${git_repository}" "${git_tag}" "${module_dir}")
 endfunction()
 
 # Download and turn on a remote module.
