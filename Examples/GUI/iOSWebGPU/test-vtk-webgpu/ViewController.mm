2884d2d126bc72bfc49b0e26752db1997ccef631
eightysix <eighty_._six@icloud.com>
2026-06-18 20:54:28 +0200

    Added some gestures to iOS WebGPU example

--- Examples/GUI/iOSWebGPU/test-vtk-webgpu/ViewController.mm
+++ Examples/GUI/iOSWebGPU/test-vtk-webgpu/ViewController.mm
@@ -13,13 +13,16 @@
 #include "vtkWebGPURenderer.h"
 #include "vtkWebGPURenderWindow.h"
 
-@interface ViewController ()
+@interface ViewController () <UIGestureRecognizerDelegate>
 {
   vtkNew<vtkIOSHardwareWindow> _hw;
   vtkNew<vtkWebGPURenderWindow> _renWin;
   vtkNew<vtkWebGPURenderer> _renderer;
   vtkNew<vtkRenderWindowInteractor> _iren;
 }
+@property (nonatomic, strong) UIPinchGestureRecognizer *pinchRecognizer;
+@property (nonatomic, strong) UIPanGestureRecognizer *panRecognizer;
+@property (nonatomic, strong) UIRotationGestureRecognizer *rotationRecognizer;
 @end
 
 @implementation ViewController
@@ -27,7 +30,6 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  // Setup VTK scene
   vtkNew<vtkConeSource> cone;
   cone->SetResolution(128);
   vtkNew<vtkWebGPUPolyDataMapper> mapper;
@@ -47,28 +49,134 @@
   _renWin->SetWindowName("test-vtk-webgpu");
   _iren->SetRenderWindow(_renWin);
 
-  // Wire up the hardware window before Render()
   _renWin->SetHardwareWindow(_hw);
   _hw->SetInteractor(_iren);
 
-  // Set multitouch interactor style
   vtkNew<vtkInteractorStyleMultiTouchCamera> style;
   _iren->SetInteractorStyle(style);
 
-  // Initialize the interactor
   _iren->Initialize();
 
-  // Render triggers hardware window Create()
   _renderer->ResetCamera();
   _renWin->Render();
 
-  // Embed the VTK view into our view hierarchy
-  UIView* vtkView = _hw->GetViewId();
+  UIView *vtkView = _hw->GetViewId();
   if (vtkView) {
     vtkView.frame = self.view.bounds;
     vtkView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [self.view addSubview:vtkView];
+    [self setupGestureRecognizersOnView:vtkView];
   }
+}
+
+- (void)setupGestureRecognizersOnView:(UIView *)view {
+  self.pinchRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self
+                                                                    action:@selector(handlePinch:)];
+  self.pinchRecognizer.delegate = self;
+  [view addGestureRecognizer:self.pinchRecognizer];
+
+  self.panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self
+                                                                action:@selector(handlePan:)];
+  self.panRecognizer.delegate = self;
+  self.panRecognizer.maximumNumberOfTouches = 1;
+  [view addGestureRecognizer:self.panRecognizer];
+
+  self.rotationRecognizer = [[UIRotationGestureRecognizer alloc] initWithTarget:self
+                                                                          action:@selector(handleRotation:)];
+  self.rotationRecognizer.delegate = self;
+  [view addGestureRecognizer:self.rotationRecognizer];
+}
+
+- (void)forwardTouchPosition:(UIGestureRecognizer *)recognizer {
+  CGPoint p = [recognizer locationInView:recognizer.view];
+  CGFloat height = recognizer.view.bounds.size.height;
+  _iren->SetEventInformation((int)p.x, (int)(height - p.y), 0, 0, 0, 0, 0);
+}
+
+- (void)handlePinch:(UIPinchGestureRecognizer *)recognizer {
+  [self forwardTouchPosition:recognizer];
+
+  CGFloat scale = recognizer.scale;
+  scale = MAX(-3.0, MIN(3.0, scale));
+  _iren->SetScale(scale);
+
+  switch (recognizer.state) {
+    case UIGestureRecognizerStateBegan:
+      _iren->StartPinchEvent();
+      break;
+    case UIGestureRecognizerStateChanged:
+      _iren->PinchEvent();
+      break;
+    case UIGestureRecognizerStateEnded:
+    case UIGestureRecognizerStateCancelled:
+      _iren->EndPinchEvent();
+      break;
+    default:
+      break;
+  }
+
+  _renWin->Render();
+}
+
+- (void)handlePan:(UIPanGestureRecognizer *)recognizer {
+  [self forwardTouchPosition:recognizer];
+
+  CGPoint translation = [recognizer translationInView:recognizer.view];
+  CGFloat scale = self.view.contentScaleFactor;
+  double t[2] = {scale * translation.x, -scale * translation.y};
+  _iren->SetTranslation(t);
+
+  switch (recognizer.state) {
+    case UIGestureRecognizerStateBegan:
+      _iren->StartPanEvent();
+      break;
+    case UIGestureRecognizerStateChanged:
+      _iren->PanEvent();
+      break;
+    case UIGestureRecognizerStateEnded:
+    case UIGestureRecognizerStateCancelled:
+      _iren->EndPanEvent();
+      break;
+    default:
+      break;
+  }
+
+  _renWin->Render();
+}
+
+- (void)handleRotation:(UIRotationGestureRecognizer *)recognizer {
+  [self forwardTouchPosition:recognizer];
+
+  double angle = -[recognizer rotation] * 180.0 / M_PI;
+  _iren->SetRotation(angle);
+
+  switch (recognizer.state) {
+    case UIGestureRecognizerStateBegan:
+      _iren->StartRotateEvent();
+      break;
+    case UIGestureRecognizerStateChanged:
+      _iren->RotateEvent();
+      break;
+    case UIGestureRecognizerStateEnded:
+    case UIGestureRecognizerStateCancelled:
+      _iren->EndRotateEvent();
+      break;
+    default:
+      break;
+  }
+
+  _renWin->Render();
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)a
+    shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)b {
+  BOOL pinchOrRotation =
+      ([a isKindOfClass:[UIPinchGestureRecognizer class]] ||
+       [a isKindOfClass:[UIRotationGestureRecognizer class]]);
+  BOOL otherIsSame =
+      ([b isKindOfClass:[UIPinchGestureRecognizer class]] ||
+       [b isKindOfClass:[UIRotationGestureRecognizer class]]);
+  return pinchOrRotation && otherIsSame;
 }
 
 - (void)viewDidLayoutSubviews {
