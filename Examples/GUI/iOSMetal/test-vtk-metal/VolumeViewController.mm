8605337f30a9d4dc1cd37457d0911bc61f600afb
eightysix <eighty_._six@icloud.com>
2026-07-13 22:12:14 +0200

    Stepper for sample distance in iOS VR examples

--- Examples/GUI/iOSMetal/test-vtk-metal/VolumeViewController.mm
+++ Examples/GUI/iOSMetal/test-vtk-metal/VolumeViewController.mm
@@ -8,11 +8,83 @@
 #include "vtkVolumeProperty.h"
 #include "vtkMetalGPUVolumeRayCastMapper.h"
 #include "vtkMetalRenderer.h"
+#include "vtkIOSMetalRenderWindow.h"
+
+@interface VolumeViewController ()
+@property (nonatomic, strong) UILabel* sampleDistanceLabel;
+@property (nonatomic, strong) UIStepper* sampleDistanceStepper;
+@property (nonatomic, assign) float currentSampleDistance;
+@end
 
 @implementation VolumeViewController
 
-- (void)setupVTKPipeline
-{
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  [self setupUI];
+}
+
+- (void)setupUI {
+  // Create label for sample distance
+  self.sampleDistanceLabel = [[UILabel alloc] init];
+  self.sampleDistanceLabel.textColor = [UIColor whiteColor];
+  self.sampleDistanceLabel.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.7];
+  self.sampleDistanceLabel.textAlignment = NSTextAlignmentCenter;
+  self.sampleDistanceLabel.layer.cornerRadius = 8;
+  self.sampleDistanceLabel.clipsToBounds = YES;
+  self.sampleDistanceLabel.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.view addSubview:self.sampleDistanceLabel];
+
+  // Create stepper for sample distance
+  self.sampleDistanceStepper = [[UIStepper alloc] init];
+  self.sampleDistanceStepper.minimumValue = 0.1;
+  self.sampleDistanceStepper.maximumValue = 5.0;
+  self.sampleDistanceStepper.stepValue = 0.1;
+  self.sampleDistanceStepper.value = 1.0;  // Will be updated after VTK pipeline setup
+  self.sampleDistanceStepper.tintColor = [UIColor whiteColor];
+  [self.sampleDistanceStepper addTarget:self
+                                action:@selector(sampleDistanceChanged:)
+                      forControlEvents:UIControlEventValueChanged];
+  self.sampleDistanceStepper.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.view addSubview:self.sampleDistanceStepper];
+
+  // Create label for title
+  UILabel* titleLabel = [[UILabel alloc] init];
+  titleLabel.text = @"Sample Distance:";
+  titleLabel.textColor = [UIColor whiteColor];
+  titleLabel.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.7];
+  titleLabel.textAlignment = NSTextAlignmentCenter;
+  titleLabel.layer.cornerRadius = 8;
+  titleLabel.clipsToBounds = YES;
+  titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.view addSubview:titleLabel];
+
+  // Layout constraints
+  [NSLayoutConstraint activateConstraints:@[
+    [titleLabel.bottomAnchor
+        constraintEqualToAnchor:self.sampleDistanceStepper.topAnchor
+                       constant:-10],
+    [titleLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+    [titleLabel.widthAnchor constraintEqualToConstant:150],
+    [titleLabel.heightAnchor constraintEqualToConstant:30],
+
+    [self.sampleDistanceStepper.centerXAnchor
+        constraintEqualToAnchor:self.view.centerXAnchor],
+    [self.sampleDistanceStepper.bottomAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor
+                       constant:-20],
+
+    [self.sampleDistanceLabel.topAnchor
+        constraintEqualToAnchor:self.sampleDistanceStepper.bottomAnchor
+                       constant:10],
+    [self.sampleDistanceLabel.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+    [self.sampleDistanceLabel.widthAnchor constraintEqualToConstant:100],
+    [self.sampleDistanceLabel.heightAnchor constraintEqualToConstant:30]
+  ]];
+
+  [self updateSampleDistanceLabel];
+}
+
+- (void)setupVTKPipeline {
   vtkMetalRenderer* renderer = static_cast<vtkMetalRenderer*>([self renderer]);
 
   vtkNew<vtkRTAnalyticSource> source;
@@ -25,6 +97,13 @@
 
   vtkNew<vtkMetalGPUVolumeRayCastMapper> mapper;
   mapper->SetInputConnection(source->GetOutputPort());
+  mapper->SetAutoAdjustSampleDistances(0);  // Disable auto-adjust
+  mapper->SetSampleDistance(1.0);  // Set initial sample distance
+
+  // Store mapper for later use
+  self.currentSampleDistance = 1.0;
+  self.sampleDistanceStepper.value = self.currentSampleDistance;
+  [self updateSampleDistanceLabel];
 
   vtkNew<vtkColorTransferFunction> colorFunc;
   colorFunc->AddRGBPoint(0.0, 0.0, 0.0, 0.0);
@@ -51,6 +130,35 @@
   volume->SetProperty(property);
 
   renderer->AddVolume(volume);
+}
+
+- (void)sampleDistanceChanged:(UIStepper*)sender {
+  self.currentSampleDistance = sender.value;
+  [self updateSampleDistanceLabel];
+  [self updateMapperSampleDistance];
+}
+
+- (void)updateSampleDistanceLabel {
+  self.sampleDistanceLabel.text =
+      [NSString stringWithFormat:@"%.1f", self.currentSampleDistance];
+}
+
+- (void)updateMapperSampleDistance {
+  vtkMetalRenderer* renderer = static_cast<vtkMetalRenderer*>([self renderer]);
+
+  // Get the volume from the renderer
+  vtkVolumeCollection* volumeCollection = renderer->GetVolumes();
+  volumeCollection->InitTraversal();
+  vtkVolume* volume = volumeCollection->GetNextVolume();
+
+  if (volume) {
+    vtkMetalGPUVolumeRayCastMapper* mapper =
+        static_cast<vtkMetalGPUVolumeRayCastMapper*>(volume->GetMapper());
+    if (mapper) {
+      mapper->SetSampleDistance(self.currentSampleDistance);
+      static_cast<vtkIOSMetalRenderWindow*>([self renderWindow])->Render();
+    }
+  }
 }
 
 @end
